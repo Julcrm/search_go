f74f0d855c686126dae533e0cf6fc6fda25e6454
Julien Castellano <julien@MacBook-Pro-de-Julien.local>
2025-02-19 11:47:22 +0100

    bouton déco

--- dash_admin.py
+++ dash_admin.py
@@ -27,7 +27,7 @@ def dash_admin():
     st.write("")
     st.write("")
     st.write("")
-    col1, col2, col3 = st.columns([0.3,0.5,3])
+    col1, col2, col3 = st.columns([0.3,0.2,2])
 
     with col2:
         if st.button("Déconnexion"):

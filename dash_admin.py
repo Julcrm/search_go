a424621f550c0ed7cf88ad48f5b84a3d22365625
Julien Castellano <julien@MacBook-Pro-de-Julien.local>
2025-02-19 15:51:03 +0100

    bouton dash admin

--- dash_admin.py
+++ dash_admin.py
@@ -27,17 +27,17 @@ def dash_admin():
     st.write("")
     st.write("")
     st.write("")
-    col1, col2 = st.columns([0.4,0.5])
 
-    with col1:
-        if st.button("Déconnexion"):
-            st.session_state["authenticated"] = False
-            st.session_state["current_user"] = None
-            st.session_state["current_page"] = "Landing"
-            st.rerun()
 
-    with col2:
-        st.markdown("<h5 style='text-align: center;'>Bonjour patron , voici un petit tour d'horizon de l'utilisation de Search & GO</h5>",unsafe_allow_html=True)
+
+    if st.button("Déconnexion"):
+        st.session_state["authenticated"] = False
+        st.session_state["current_user"] = None
+        st.session_state["current_page"] = "Landing"
+        st.rerun()
+
+
+    st.markdown("<h5 style='text-align: center;'>Bonjour patron , voici un petit tour d'horizon de l'utilisation de Search & GO</h5>",unsafe_allow_html=True)
     st.write("")
     st.divider()
 
